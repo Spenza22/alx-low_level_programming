08128e5f0fa80de58d2c5ea78d31a5528c699d55
Spenza22 <spencernweke@gmail.com>
2022-10-13 20:31:03 -0700

    Task 4 Modifications

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ *main - This is the short description
+ *This is the entry point of the main function
+ *Return: Description of the returned value?
+ */
 int main(void)
 {
 	char value[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(value);
 	return (0);
 }
