fa6679acda64c51a475eab6fd70a9b21d989c406
Spenza22 <spencernweke@gmail.com>
2022-10-13 20:39:48 -0700

    Hello World Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the main function teaching how to use the printf() function
+ *Return: And I have to document its return function.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
