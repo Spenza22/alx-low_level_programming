49f54c18a7e50f08483ca845adc3bb66c1723e2d
Spenza22 <spencernweke@gmail.com>
2022-10-13 20:03:51 -0700

    Modification to task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-	puts(char "\"Programming is like building a multilingual puzzle,");
+	puts(const char "\"Programming is like building a multilingual puzzle,");
 	return (0);
 }
