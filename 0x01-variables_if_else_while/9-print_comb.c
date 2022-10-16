b3a6e3de560facdd0f0edd52390ee89c0a31b4c6
Spenza22 <spencernweke@gmail.com>
2022-10-15 17:11:37 -0700

    This is the last compulsory Task - Task 9. I hope it works

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - This is the main Entry point
+ * Return: The main function returns zero
+ */
+int main(void)
+{
+	/* int Ualpha = 97;*/
+	int space = 32;
+	int comma = 44;
+	int alpha = 48;
+	/* newline == 10 || null == */
+	while (alpha <= 57)
+	{
+		if (alpha == 57)
+		{
+			comma = 0;
+			space = 10;
+		}
+		putchar(alpha);
+		putchar(comma);
+		putchar(space);
+		alpha++;
+	}
+	/* putchar(10); */
+	return (0);
+}
