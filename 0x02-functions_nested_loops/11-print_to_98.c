56bb8fd341c9fadf52f788b8a2d6b8863a8a2b86
Spenza22 <spencernweke@gmail.com>
2022-10-19 14:43:12 -0700

    Functions Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
-*print_to_98 - This functions prints digits from
-*the entry point up to 98
-*Return: No return value
-*@n: This parameter accepts integer values
-*/
+ * print_to_98 - Prints all numbers down to 98
+ * Return: No return value
+ * @n: Accepts integer values
+ */
 void print_to_98(int n)
 {
 	int i = n;
 
-	for ( ; i <= 98; i++)
+	if (i <= 98)
 	{
-		/* code */
-		printf("%d", i);
-		if (i == 98)
+		for ( ; i <= 98; i++)
 		{
 			/* code */
-			continue;
+			printf("%d", i);
+			if (i == 98)
+			{
+				/* code */
+				continue;
+			}
+			else
+				printf(", ");
 		}
-		else
-			printf(", ");
+	}
+	else
+	{
+		for ( ; i >= 98; i--)
+		{
+			/* code */
+			printf("%d", i);
+			if (i == 98)
+			{
+				/* code */
+				continue;
+			}
+			else
+				printf(", ");
+		}
 	}
 	printf("\n");
-
-
 }
