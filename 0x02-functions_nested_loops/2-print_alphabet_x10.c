fb5d3fb7f4177803c83613c20af7a93f562aa255
Spenza22 <spencernweke@gmail.com>
2022-10-19 03:36:58 -0700

    Functions Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "main.h"
 void print_alphabet_x10(void);
 /**
- * print_alphabet_x10 - .
+ * print_alphabet_x10 - Print all the alphabets in lower cases 10x
  *  Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
 	for (int i = 0; i < 10; i++)
 	{
-		
 		char myAlp = 'a';
+
 		for ( ; myAlp <= 'z'; myAlp++)
 		{
 			_putchar(myAlp);
