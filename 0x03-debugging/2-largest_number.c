3ce5f49a3d45186cb052d51cfa9b6155af5d8e93
Spenza22 <spencernweke@gmail.com>
2022-10-20 07:37:12 -0700

    Task 2 of debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,24 +1,4 @@
 #include "main.h"
-/**
- * main - prints the largest of 3 integers
- * Return: 0
- */
-
-int main(void)
-{
-	int a, b, c;
-	int largest;
-
-	a = 0;
-	b = 0;
-	c = 0;
-
-	largest = largest_number(a, b, c);
-
-	printf("%d is the largest number\n", largest);
-
-	return (0);
-}
 
 /**
  * largest_number - returns the largest of 3 numbers
