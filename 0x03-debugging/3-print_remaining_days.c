749121ebcbb5ff3d3c2468e7f8ad96856896d1b7
Spenza22 <spencernweke@gmail.com>
2022-10-20 20:38:23 -0700

    Debudding Task3-1

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -22,7 +22,6 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 
-		/* printf("%d is a Leap Year", year); */
 	}
 	else
 	{
@@ -37,6 +36,5 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 
-		/* printf("%d is not a Leap Year", year); */
 	}
 }
