d0fa7626e0b380819ece8a2557dcd7b6f2941ca1
Spencer Nweke <spencernweke@gmail.com>
2022-10-22 01:01:06 +0100

    more functions task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - checks for number/digits
+ * @a: First int argument
+ * @b: Second int argument
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
