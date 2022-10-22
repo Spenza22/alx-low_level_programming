dc7e2c7761fadd64ce3ab9aad63f49dc3430bffe
Spencer Nweke <spencernweke@gmail.com>
2022-10-22 01:33:21 +0100

    more functions task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		/* code */
+		if (i % 15 == 0)
+		{
+			/* code */
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+
+	}
+	printf("\n");
+	return (0);
+}
