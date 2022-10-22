035d5ed560b243df27ff1dabf5a2ba21a444455e
Spencer Nweke <spencernweke@gmail.com>
2022-10-22 01:47:24 +0100

    New push for task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,11 +6,12 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for ( i = 1; i < 101; i++)
 	{
 		/* code */
 		if (i % 15 == 0)
@@ -18,16 +19,17 @@ int main(void)
 			/* code */
 			printf("FizzBuzz ");
 		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
 		else
 			printf("%d ", i);
+
 
 	}
 	printf("\n");
