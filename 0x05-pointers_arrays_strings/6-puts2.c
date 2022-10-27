686a85c9d30256e7a0d379e97d002acdca1d18cd
Spencer Nweke <spencernweke@gmail.com>
2022-10-27 16:34:17 +0100

    Pointers task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,17 @@
  * Return: A void function
  */
 
+
 void puts2(char *str)
 {
 	int i = 0;
 
-	for ( ; str[i] != '\0'; i += 2)
+	for ( ; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
